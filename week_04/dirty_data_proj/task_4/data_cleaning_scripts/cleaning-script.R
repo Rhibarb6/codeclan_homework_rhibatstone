7282c1ec599d3d95727b2e41ef38789b25c6dd46
Rhi <rbatstone94@gmail.com>
2020-03-18 18:13:30 +0000

    Homework update

--- week_04/dirty_data_proj/task_4/data_cleaning_scripts/cleaning-script.R
+++ week_04/dirty_data_proj/task_4/data_cleaning_scripts/cleaning-script.R
@@ -2,12 +2,9 @@ library(tidyverse)
 library(janitor)
 library(readxl)
 
-candy_2015 <- read_excel("candy_ranking_data/boing-boing-candy-2015.xlsx")
-candy_2016 <- read_excel("candy_ranking_data/boing-boing-candy-2016.xlsx")
-candy_2017 <- read_excel("candy_ranking_data/boing-boing-candy-2017.xlsx")
 
-
-candy_2015_clean <- candy_2015 %>% 
+candy_2015_clean <- read_excel("candy_ranking_data/boing-boing-candy-2015.xlsx"
+    ) %>% 
   pivot_longer(
     cols = starts_with("["), 
     names_to = "candy",
@@ -19,25 +16,52 @@ candy_2015_clean <- candy_2015 %>%
     sep = " "
     ) %>%
   clean_names() %>% 
-  select(date, time, how_old_are_you, candy, response)
+  rename(age = how_old_are_you) %>% 
+  select(date, time, age, candy, response)
 
 ##2016
-candy_2015_clean <- candy_2015 %>% 
+candy_2016_clean <- read_excel("candy_ranking_data/boing-boing-candy-2016.xlsx"
+    ) %>% 
   pivot_longer(
     cols = starts_with("["), 
     names_to = "candy",
     values_to = "response"
-  ) %>% 
+    ) %>% 
   separate(
     Timestamp, 
     c("date", "time"), 
     sep = " "
-  ) %>%
+    ) %>%
   clean_names() %>% 
-  select(date, time, age, candy, response)
+  rename(
+    age = how_old_are_you, 
+    gender = your_gender, 
+    country = which_country_do_you_live_in
+    ) %>% 
+  select(date, time, age, gender, country, candy, response)
+
+## 2017
+
+candy_2017_clean <- read_excel("candy_ranking_data/boing-boing-candy-2017.xlsx"
+    ) %>%
+  pivot_longer(
+    cols = starts_with("Q6"),
+    names_to = "candy",
+    values_to = "response"
+    ) %>% 
+  clean_names() %>%
+  rename(
+    age = q3_age,
+    gender = q2_gender,
+    country = q4_country
+    ) %>% 
+  select(internal_id, age, gender, country, candy, response)
+
+candy_clean <- bind_rows(candy_2015_clean, candy_2016_clean, candy_2017_clean)
 
 
-dim(candy_2016)
-dim(candy_2017)
-names(candy_2016)
-names(candy_2017)
+
+
+
+
+
